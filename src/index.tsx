07a25bad0f10d293422e9d0214ba1176e6a7d8d2
Anderson Luis Silva <andersonluis87@gmail.com>
2022-04-07 23:16:28 -0300

    Adding mirageJS and React-Modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App }  from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+    
+    this.get('/transactions', () => {
+      return [
+        { 
+          id: 1,
+          title: 'Transaction 1',
+          amount: 100,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        },
+        { 
+          id: 2,
+          title: 'Transaction 2',
+          amount: 1000,
+          type: 'income',
+          category: 'salary',
+          createdAt: new Date()
+        }
+
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
