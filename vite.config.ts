c236052b8c2b6b4c944f64bcf286c2d782b2c6c3
Anderson Luis Silva <andersonluis87@gmail.com>
2022-04-19 23:32:47 -0300

    Migrating CRA to VITE

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import svgrPlugin from 'vite-plugin-svgr'
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+})
