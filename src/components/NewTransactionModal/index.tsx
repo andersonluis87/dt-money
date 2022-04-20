552cf6d034027ee7c71de1c21354b907aec4a5d4
Anderson Luis Silva <andersonluis87@gmail.com>
2022-04-20 00:16:38 -0300

    Adding Seeds and fetching data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
@@ -13,7 +14,7 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionModalProps ) {
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
 
   const [type, setType] = useState('deposit');
@@ -21,7 +22,9 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log(title, value, category, type);
+    const data = { title, amount, category, type };
+
+    api.post('/transactions', data);
   }
 
   return (
@@ -47,8 +50,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import { createRoot  } from 'react-dom/client';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App }  from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Lazer',
+          amount: 150,
+          type: 'deposit',
+          category: 'lazer',
+          createdAt: new Date('2022-03-12T03:24:00'),
+        },
+        {
+          id: 2,
+          title: 'Alimentação',
+          amount: 100,
+          type: 'withdraw',
+          category: 'alimentação',
+          createdAt: new Date('2022-04-01T15:24:00'),
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          amount: 1300,
+          type: 'withdraw',
+          category: 'casa',
+          createdAt: new Date('2022-04-10T12:23:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
     
     this.get('/transactions', () => {
-      return [
-        { 
-          id: 1,
-          title: 'Transaction 1',
-          amount: 100,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        },
-        { 
-          id: 2,
-          title: 'Transaction 2',
-          amount: 1000,
-          type: 'income',
-          category: 'salary',
-          createdAt: new Date()
-        }
+      return this.schema.all('transaction')
+    })
 
-      ]
+    this.post('/transactions', (schema, request) => {
+      const data = {
+        id: Math.floor(Math.random() * 100),
+        createdAt: new Date(),
+        ...JSON.parse(request.requestBody)
+      }
+
+      return schema.create('transaction', data)
     })
   }
 })
