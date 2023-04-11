bef8b0c794ad2334718ce09944b42e87acf7d3fd
Anderson Luis Silva <andersonluis87@gmail.com>
2023-04-11 05:18:22 -0300

    Refactoring useContext

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
-import { Container } from "./styles";
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
 import { useTransactions } from '../../hooks/useTransactions';
 import { formatCurrency } from '../../utils';
+import { Container } from "./styles";
 
 export function Summary() {
   const { transactions } = useTransactions();
